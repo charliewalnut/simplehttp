8551b4d05b9b88fa27c4aff6b13181e49799dfd5
James Robinson <jamesr@chromium.org>
2013-09-23 17:11:47 -0700

    serve from files

--- main.go
+++ main.go
@@ -1,57 +1,37 @@
 package main
 
 import (
-  "fmt"
+	"bufio"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 type handler struct{}
 
-const firstChunk = `<!DOCTYPE html>
-<head><title>Test of HTML 12.2.5.4.8 script parser spin behavior</title>
-<link href="" rel="stylesheet" type="text/css"></link>
-</head>
-<script>
-var order = "";
-function foo() { order += "foo "; }
-function bar() { order += "bar "; }
-
-function go() {
-  order += "go ";
-
-  var cssURL = "http://1.cuzillion.com/bin/resource.cgi?type=css&sleep=2&n=1"
-  var sheet = document.createElement('link');
-  sheet.rel = "stylesheet";
-  sheet.type = "text/css";
-  sheet.href = cssURL;
-  document.head.appendChild(sheet);
-  window.setTimeout(foo, 0);
-  document.write("<script>order += 'inline '</scr"+"ipt> script closed");
-  bar();
+func serve(filename string, w http.ResponseWriter) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rdr := bufio.NewReader(file)
+	firstChunk, err := rdr.ReadString(0)
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, firstChunk)
 }
-
-window.onload = function() {
-  order += "load ";
-  window.alert(order);
-}
-</script>
-<body>
-start of &lt;body&gt;
-<img src="http://localhost:8000/404.gif" onerror="go()">`
-
-const secondChunk = `end of &lt;body&gt;
-</body>
-`
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, firstChunk)
-  if f, ok := w.(http.Flusher); ok {
-    f.Flush()
-  }
-  time.Sleep(5 * time.Second)
-  fmt.Fprint(w, secondChunk)
+	serve("first_chunk.txt", w)
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
+	time.Sleep(500 * time.Millisecond)
+	serve("second_chunk.txt", w)
 }
 
 func main() {
